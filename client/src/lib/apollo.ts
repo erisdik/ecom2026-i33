0c730c9688faaf3f502e921c6959aa680c055e73
erisdik <erisdik@duck.com>
2025-12-11 14:41:30 +0000

    add env

--- client/src/lib/apollo.ts
+++ client/src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: '/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URI || '/graphql',
 });
 
 export const client = new ApolloClient({
