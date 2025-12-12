86669daae5b38c062079cde4d5e50414426ec44e
erisdik <erisdik@duck.com>
2025-12-12 02:19:17 +0000

    deploy

--- client/src/lib/apollo.ts
+++ client/src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_URI || '/graphql',
+  uri: 'https://creativu.es/graphql',
 });
 
 export const client = new ApolloClient({
